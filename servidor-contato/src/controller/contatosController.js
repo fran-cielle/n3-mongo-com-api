254ad3132a2a256f8bef94e7839a5380ce7bbdfd
francielle <nasc.francielle@gmail.com>
2019-11-18 21:10:26 -0300

    Inserindo dados do body no bd

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -1,4 +1,5 @@
-const model = require("../model/contatos")
+//connecta com o bd 
+const contatosCollection = require("../model/contatoSchema")
 
 const getAll = (request, response) => {
   console.log(request.url)
@@ -6,20 +7,26 @@ const getAll = (request, response) => {
 };
 
 const add = (request, response) => {
-  let contato = request.body
-  let baseDados = model.agenda.contatos
-  contato.id = Math.random().toString(36).substr(-8)
+  //novo objeto para nossa nova coleção
+  const contatoDoBody = request.contatoDoBody//objeto json que veio no body
+  const contato = new contatosCollection(contatoDoBody) //pega o objeto do body e transforma em um objeto para o banco| o new é uma função construtora
 
-  if (!contato.nome || !contato.dataNascimento || !contato.celular) {
-    response.status(400).send("Dados inválidos");
-  } else {
-    if (baseDados.find(dado => dado.nome === contato.nome)) {
-      response.status(400).send("Contato já cadastrado")
-    } else {
-      model.agenda.contatos.push(contato)
-      response.status(201).send(contato)
-    }
-  }
+  contato.save()
+
+  // let contato = request.body
+  // let baseDados = model.agenda.contatos
+  // contato.id = Math.random().toString(36).substr(-8)
+
+  // if (!contato.nome || !contato.dataNascimento || !contato.celular) {
+  //   response.status(400).send("Dados inválidos");
+  // } else {
+  //   if (baseDados.find(dado => dado.nome === contato.nome)) {
+  //     response.status(400).send("Contato já cadastrado")
+  //   } else {
+  //     model.agenda.contatos.push(contato)
+  //     response.status(201).send(contato)
+  //   }
+  // }
 
 }
 
---
+++ servidor-contato/src/model/contatoSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//objeto com nossos campos tem que fazer para todos os campos
+const contatoSchema = new Schema({
+    _id:{
+        type: mongoose.Schema.Types.ObjectId, //tipos dos dados
+        auto: true, //é autogerado
+        required: true // é obrigatorio
+    },
+    nome:{
+        type: String,
+        required: true
+    },
+    celular:{
+        type: String,
+        required: true
+    }
+})
+
+//cria um obj de coleção com todos os métodos
+const contatosColletion = mongoose.model('contato', contatoSchema)
+
+module.exports = contatosColletion
+
